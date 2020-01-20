4fc32f876eea0d28b2c4637242730d661f6c119f
Freeman Shadowblade <50771819+juju-e@users.noreply.github.com>
2020-01-20 15:51:37 +0000

    Pushed original file

--- py_im_change.py
+++ py_im_change.py
@@ -6,18 +6,20 @@ def change_images(path_dir,save_to,exists):
          os.mkdir(save_to)
    images=os.listdir(path)
    for image in images:
-      if image.size[0]<image.size[1]:
+      image_clone=image
+      image=Image.open(path+"/"+image_clone)
+      if image.size[0]>image.size[1]:
          width=400
-         percentage=width/float(img.size[0])
-         height=int((float(img.size[1])*float(percentage)))
-         img_res=Image.open(path+"/"+image).resize((width,height),Image.ANTIALIAS)
-         img_res.save(save_to+"/"+image)
+         percentage=width/float(image.size[0])
+         height=int((float(image.size[1])*float(percentage)))
+         img_res=Image.open(path+"/"+image_clone).resize((width,height),Image.ANTIALIAS)
+         img_res.save(save_to+"/"+image_clone)
       else:
          height=400
-         percentage=height/float(img.size[1])
-         width=int((float(img.size[0])*float(percentage)))
-         img_res=Image.open(path+"/"+image).resize((width,height),Image.ANTIALIAS)
-         img_res.save(save_to+"/"+image)
+         percentage=height/float(image.size[1])
+         width=int((float(image.size[0])*float(percentage)))
+         img_res=Image.open(path+"/"+image_clone).resize((width,height),Image.ANTIALIAS)
+         img_res.save(save_to+"/"+image_clone)
          height=400
       print("[***]finished[***]")
 path=input("[*]enter the path of the files to change----->>")
