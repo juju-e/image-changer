477618e23a8423d8a1e732ca8995a86b410e05ac
Freeman Shadowblade <50771819+juju-e@users.noreply.github.com>
2020-01-20 15:38:32 +0000

    added original file

--- py_im_change.py
+++ py_im_change.py
@@ -6,9 +6,20 @@ def change_images(path_dir,save_to,exists):
          os.mkdir(save_to)
    images=os.listdir(path)
    for image in images:
-      img_res=Image.open(path+"/"+image).resize((400,400),Image.ANTIALIAS)
-      img_res.save(save_to+"/"+image)
-   print("[***]finished[***]")
+      if image.size[0]<image.size[1]:
+         width=400
+         percentage=width/float(img.size[0])
+         height=int((float(img.size[1])*float(percentage)))
+         img_res=Image.open(path+"/"+image).resize((width,height),Image.ANTIALIAS)
+         img_res.save(save_to+"/"+image)
+      else:
+         height=400
+         percentage=height/float(img.size[1])
+         width=int((float(img.size[0])*float(percentage)))
+         img_res=Image.open(path+"/"+image).resize((width,height),Image.ANTIALIAS)
+         img_res.save(save_to+"/"+image)
+         height=400
+      print("[***]finished[***]")
 path=input("[*]enter the path of the files to change----->>")
 save_dir=input("[#]enter the name of the directory the files will be saved to------>>")
 if (os.path.exists(save_dir)):
